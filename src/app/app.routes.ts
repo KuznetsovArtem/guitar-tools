e7126a1a81032cc45dd8e6d46befa33bcbcaa98f
artem <artem@gypost.com>
2016-07-05 16:21:36 +0300

    Init commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {provideRouter, RouterConfig} from '@angular/router';
+
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {FingerboardComponent} from "./fingerboard/fingerboard.component";
+
+export const routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'board', component: FingerboardComponent }
+];
+
+export const APP_ROUTER_PROVIDERS = [
+    provideRouter(routes)
+];
---
+++ src/app/fingerboard/fingerboard.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by askuznetsov on 7/5/2016.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'fingerboard',
+    templateUrl: './app/fingerboard/fingerboard.component.html'
+})
+
+export class FingerboardComponent {
+    
+}
